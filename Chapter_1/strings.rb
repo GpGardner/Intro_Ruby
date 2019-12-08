f8b52a7f689f115baacf835aff1826ddbaa576ba
George G <GpGardner@yahoo.com>
2019-12-08 16:56:41 -0500

    Finished strings.rb

---
+++ Chapter_1/strings.rb
@@ -0,0 +1,54 @@
+#(1)
+fname = 'George'
+lname = 'Gardner'
+
+puts "lastname, #{lname} : firstname, #{fname}"
+
+"George" + "Gardner"
+
+#(2)
+
+#4295
+thousands = 4295 / 1000
+hundreds = 4295 % 1000 / 100
+tens = 4295 % 1000 % 100 / 10
+ones = 4295 % 1000 % 100 % 10
+
+#(3)
+movies = {:Avengers => '2012', :Age_of_Ultron => '2015', :Infinity_War => '2018', :Endgame => '2019'}
+
+puts movies[:Avengers]
+puts movies[:Age_of_Ultron]
+puts movies[:Infinity_War]
+puts movies[:Endgame]
+
+#(4)
+dates = [2012, 2015, 2018, 2019]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+
+#(5)
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+
+#(6)
+puts 404.4*404.4
+puts 1337.3*1337.3
+puts 12.21*12.21
+
+#(7)
+#Ending a hash with a ')' instead of '}'
+
+
+
+
+
+
+
+
